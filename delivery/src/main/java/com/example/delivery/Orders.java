a4dd91e00150300d22da4e956c59127cc4b80b16
fifol <fkucharski@student.agh.edu.pl>
2024-12-22 18:57:47 +0100

    commit 2

--- delivery/src/main/java/com/example/delivery/Orders.java
+++ delivery/src/main/java/com/example/delivery/Orders.java
@@ -1,4 +1,10 @@
 package com.example.delivery;
 
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Orders {
 }
