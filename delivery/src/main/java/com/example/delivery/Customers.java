942c4211c2104e3eeebe3556d9c4342b997044c8
fifol <fkucharski@student.agh.edu.pl>
2024-12-23 19:14:47 +0100

    stage closing and class addcustomers

--- delivery/src/main/java/com/example/delivery/Customers.java
+++ delivery/src/main/java/com/example/delivery/Customers.java
@@ -2,6 +2,8 @@ package com.example.delivery;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -10,12 +12,13 @@ import java.io.IOException;
 public class Customers {
     @FXML
     private Button backButton;
+
     public void onbackButton(ActionEvent actionEvent) {
-        try{
+        try {
             HelloApplication.changescene("after_login.fxml");
             Stage stage = (Stage) backButton.getScene().getWindow();
             stage.close();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -42,4 +45,14 @@ public class Customers {
     public void onnextButton(ActionEvent actionEvent) {
     }
 
+    public void onaddcostumerButton(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addcostumer.fxml"));
+        Stage add_costumer_stage = new Stage();
+        add_costumer_stage.setTitle("Add Costumer");
+        add_costumer_stage.setResizable(false);
+        add_costumer_stage.setScene(new Scene(fxmlLoader.load(), 600, 400));
+        add_costumer_stage.show();
+
+
+    }
 }
