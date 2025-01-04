2770e2ee83a18363f819c8e89f694dee7cd42aec
fifol <fkucharski@student.agh.edu.pl>
2025-01-04 01:20:51 +0100

    Added vehicle display and corrected ErrorAlert as a static method in main class

---
+++ delivery/src/main/java/com/example/delivery/SearchVehicle.java
@@ -0,0 +1,44 @@
+package com.example.delivery;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SearchVehicle {
+    @FXML
+    public TextField vehicleidfield;
+    @FXML
+    public TextField typefield;
+    @FXML
+    public TextField capacityfield;
+    @FXML
+    public Button backButton;
+    @FXML
+    public TextField loadfield;
+    @FXML
+    public TextField availablefield;
+
+    public void setInfo(String vehicleid, String type, String capacity,String isavailable, String load) {
+        vehicleidfield.setText(vehicleid);
+        typefield.setText(type);
+        capacityfield.setText(capacity);
+        availablefield.setText(load);
+    }
+
+    public void onbackButton(ActionEvent actionEvent) throws IOException {
+        try {
+            HelloApplication.changescene("vehicle.fxml");
+            Stage stage = (Stage) backButton.getScene().getWindow();
+            stage.close();
+        }catch (NullPointerException e){
+            HelloApplication.ErrorAlert("Error Occured", "Error Occured");
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
